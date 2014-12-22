09ee41e1bcd5bb7c0e4515a0e4963db35485c801
smozely <me@steve-mosley.com>
2014-12-23 12:51:30 +1300

    Adding in CORS Headers for responses.

--- src/main/java/com/smozely/app/emailer/Application.java
+++ src/main/java/com/smozely/app/emailer/Application.java
@@ -1,6 +1,7 @@
 package com.smozely.app.emailer;
 
 import com.smozely.si.management.ManagementConfig;
+import com.smozely.si.web.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Import;
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
-@Import({AwsConfig.class, IntegrationConfig.class, ManagementConfig.class})
+@Import({AwsConfig.class, IntegrationConfig.class, ManagementConfig.class, WebConfig.class})
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/smozely/si/web/WebConfig.java
@@ -0,0 +1,9 @@
+package com.smozely.si.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class WebConfig {
+}
